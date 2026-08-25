e8edcacb61a4c1675695d7bba7e2a647705acd63
MiguelMironSilva <32493489+MiguelMironSilva@users.noreply.github.com>
2026-08-25 07:18:53 -0300

    Update script.js
    Zoom mínimo e máximo corrigido.

--- script.js
+++ script.js
@@ -44,8 +44,8 @@ const MAX_HISTORY = 20;
 let zoomLevel = 1;
 let originalDisplayWidth;
 let originalDisplayHeight;
-const MIN_ZOOM = 0.25;
-const MAX_ZOOM = 1;
+const MIN_ZOOM = 1;
+const MAX_ZOOM = 4;
 const ZOOM_STEP = 0.1;
 
 //Suporte para seleção, movimento, escalonamento e rotação
